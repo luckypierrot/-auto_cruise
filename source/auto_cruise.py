029b5f3aadd4e523d06d69cd0e14b2e3e13b153c
naoto-yoshida <naoto-yoshida@hldc.co.jp>
2019-03-27 13:36:22 +0900

    自動運転処理を追加/調整

--- source/auto_cruise.py
+++ source/auto_cruise.py
@@ -27,9 +27,11 @@ PWM_RANGE            = 100
 PWM_FREQ             = 50
 POWER_OFF            = 0
 POWER_ON             = 1
-STEERING_LEFT        = 1200
-STEERING_FORWORD     = 1500
-STEERING_RIGHT       = 1800
+STEERING_LEFT        = 12000
+STEERING_FORWORD     = 15000
+STEERING_RIGHT       = 18000
+MAX_MOTOR_SPEED      = 1000
+MIN_MOTOR_SPEED      = 1000
 
 # @brief	進行方向定義
 DIR_FORWARD          = 1
@@ -41,11 +43,30 @@ DRIVE_START          = 1
 DRIVE_STOP           = 0
 
 # @brief	カメラ設定値
-CAM_SETTING_WIDTH    = 640
-CAM_SETTING_HEIGHT   = 480
+CAM_SETTING_WIDTH    = 320
+CAM_SETTING_HEIGHT   = 240
 CAM_SETTING_FPS      = 24
 
+# @brief 台形補正値
+CAM_ADJUST_WIDTH     = 80   #左右から80pix
+CAM_ADJUST_HEIGHT    = 150  #上から150px
 
+# @brief ライン領域の無視判定スレッシュ
+PIC_THRESH_MIN       = 2000
+PIC_THRESH_MAX       = 10000
+
+
+# @brief ライン角度-直進判定
+STEER_ANGLE_MIN     = -10    
+STEER_ANGLE_MAX     = 10   
+
+# @brief ライン中心のずれ
+LINE_GAP_MIN       = -10
+LINE_GAP_MAX       = 10
+
+# @brief ステアリング調整用比例ゲイン(floatで指定可能)
+COEF_CENTERING     = 0.1
+COEF_ANGLE         = 0.1
 
 
 #####################################################################
@@ -58,6 +79,8 @@ class auto_cruise:
   drive_steer          = 0
   drive_motor          = 0
   drive_active         = 0
+  drive_centering      = 0
+  sixaxis_switch       = 1
   pi                   = 0
 
 
@@ -96,9 +119,11 @@ class auto_cruise:
     self.pi.set_PWM_dutycycle(GPIO_STEERING_PWM,	0 )
 
     # 制御用パラメータ初期化
-    self.drive_steer  = Value("i", 0 )
-    self.drive_motor  = Value("i", 0 )
-    self.drive_active = Value("i", DRIVE_START )
+    self.drive_steer      = Value("i", 0 )
+    self.drive_motor      = Value("i", 0 )
+    self.drive_centering  = Value("i", 0 )
+    self.drive_active     = Value("i", DRIVE_START )
+    self.sixaxis_switch   = Value("i", 0 )
 
     print( "init complete" )
 
@@ -114,76 +139,23 @@ class auto_cruise:
 #####################################################################
   def __cruiseCntrol( self, start_pt, vec ):
 
-    #print(int(start_pt[0]), int(start_pt[1]))
-
-    # 先端の矢印を描画
     angle = math.atan2(vec[1], vec[0])
-
-    # ステアリングパラメータを取得
     if angle < 0:
         angle = math.pi + angle
     angle = angle-(math.pi/2)
-    duty = int( angle*100 )
 
-    # 中心軸からのズレをオフセットとして加算
-    offset = int( start_pt[0] )- (CAM_SETTING_WIDTH//2)
-    offset = offset//(CAM_SETTING_WIDTH//200)
-    duty += offset
+    # ラインの方向(ラジアン)からステアリングを算出(-157~157)
+    duty    = int( angle*100 )
 
-    print(duty)
-    self.drive_steer.value = duty
+    # ライン中心座標の中央からの差分を算出(-160~160pix)
+    offset  = int( start_pt[0] )- (CAM_SETTING_WIDTH//2)
 
-    #ステアリングからスピードを算出（暫定）
-    speed = 1000 - abs(duty)*4
-    print(speed)
+    # モーター速度を算出
+    speed   = MAX_MOTOR_SPEED - abs(duty)*4
 
-    self.drive_motor.value = speed
-
-
-
-
-
-#####################################################################
-#	@fn		    __drawAxis
-#	@param		start_pt：ライン矩形の中心座標(x,y)
-#	@param		vec     ：ライン矩形のベクトル
-#	@param		colour  ：ベクトル描画色
-#	@param		length  ：ベクトル長さ
-#	@brief		ベクトルの描画を行う(デバッグ処理)
-#####################################################################
-  def __drawAxis( self, img, start_pt, vec, colour, length):
-
-    # アンチエイリアス
-    CV_AA = 16
-
-    # 終了点
-    end_pt = (int(start_pt[0] + length * vec[0]), int(start_pt[1] + length * vec[1]))
-
-    # 中心を描画
-    cv2.circle(img, (int(start_pt[0]), int(start_pt[1])), 5, colour, 1)
-    print(int(start_pt[0]), int(start_pt[1]))
-
-    # 軸線を描画
-    cv2.line(img, (int(start_pt[0]), int(start_pt[1])), end_pt, colour, 1, CV_AA)
-
-    # 先端の矢印を描画
-    angle = math.atan2(vec[1], vec[0])
-
-    # ラジアンを取得(0-314)
-    duty = int( angle*100 )
-    if duty < 0:
-        duty = 314 - duty      
-    duty = (duty-157)
-    # ステアリングパラメータを決定
-    self.drive_steer.value = duty
-
-    qx0 = int(end_pt[0] - 9 * math.cos(angle + math.pi / 4));
-    qy0 = int(end_pt[1] - 9 * math.sin(angle + math.pi / 4));
-    cv2.line(img, end_pt, (qx0, qy0), colour, 1, CV_AA);
-
-    qx1 = int(end_pt[0] - 9 * math.cos(angle - math.pi / 4));
-    qy1 = int(end_pt[1] - 9 * math.sin(angle - math.pi / 4));
-    cv2.line(img, end_pt, (qx1, qy1), colour, 1, CV_AA);
+    self.drive_centering.value  = offset
+    self.drive_steer.value      = duty
+    self.drive_motor.value      = speed
 
 
 
@@ -222,6 +194,7 @@ class auto_cruise:
         Vertical   = 'inside'
 
         # 横座標検出
+        ####################################
         if box[j][0] <= 0:
           #左側
           Horizontal = 'leftside'
@@ -233,7 +206,8 @@ class auto_cruise:
           Horizontal = 'inside'
 
         # 縦座標検出
-        if box[j][1] <= 250:
+        ####################################
+        if box[j][1] <= (CAM_SETTING_HEIGHT//2):
           #上部
           Vertical = 'upperside'
         elif box[j][1] >= (CAM_SETTING_HEIGHT-10):# マージンを持つ
@@ -254,11 +228,9 @@ class auto_cruise:
     if output == 'no lane':
       for i in range(len(position)):
         if position[i][1] == 'lowerside':
-          print("lowside!!")
           output   = 'find lane'
           break
 
-    print(output)
     return output
 
 
@@ -280,55 +252,54 @@ class auto_cruise:
 
     while self.drive_active.value == DRIVE_START:  
 
-      # 50ms周期で制御
-      if 1:
+      speed = self.drive_motor.value
 
-        speed = self.drive_motor.value
-
-        # モーター正逆制御
-        if speed > 0:
-          # 前進
-          if direction != DIR_FORWARD and now_speed == 0:
-            print("forward")
-            direction = DIR_FORWARD
-            self.pi.write( GPIO_MOTOR_CTRL1, 1 )
-            self.pi.write( GPIO_MOTOR_CTRL2, 0 )
-        elif speed < 0:
-          # 後退
-          if direction != DIR_BACK and now_speed == 0:
-            print("back")
-            direction = DIR_BACK
-            self.pi.write( GPIO_MOTOR_CTRL1, 0 )
-            self.pi.write( GPIO_MOTOR_CTRL2, 1 )
-        elif speed == 0:
-          # ブレーキ
-          if direction != DIR_STOP:
-            print("brake")
-            direction = DIR_STOP
-            self.pi.write( GPIO_MOTOR_CTRL1, 0 )
-            self.pi.write( GPIO_MOTOR_CTRL2, 0 )
-
-        # モーター速度調整
-        speed = abs(speed//10)
-
-        if now_speed < speed:
-          now_speed = now_speed+1
-          if now_speed > 100:
-            now_speed = 100
-          self.pi.set_PWM_dutycycle( GPIO_MOTOR_PWM, now_speed )
-
-        elif now_speed > speed:
-          now_speed = now_speed-3
-          if now_speed < 0:
-            now_speed = 0
-          self.pi.set_PWM_dutycycle( GPIO_MOTOR_PWM, now_speed )
+      # モーター正逆制御
+      ###############################
+      if speed > 0:
+        # 前進
+        if direction != DIR_FORWARD and now_speed == 0:
+          direction = DIR_FORWARD
+          self.pi.write( GPIO_MOTOR_CTRL1, 1 )
+          self.pi.write( GPIO_MOTOR_CTRL2, 0 )
+      elif speed < 0:
+        # 後退
+        if direction != DIR_BACK and now_speed == 0:
+          direction = DIR_BACK
+          self.pi.write( GPIO_MOTOR_CTRL1, 0 )
+          self.pi.write( GPIO_MOTOR_CTRL2, 1 )
+      elif speed == 0:
+        # ブレーキ
+        if direction != DIR_STOP:
+          direction = DIR_STOP
+          self.pi.write( GPIO_MOTOR_CTRL1, 0 )
+          self.pi.write( GPIO_MOTOR_CTRL2, 0 )
 
 
-      # 1ms周期で制御
-      if 1:
-        
-        # ステアリング動作量を取得
-        duty  = STEERING_FORWORD + (self.drive_steer.value*2)      
+      # モーター速度制御
+      ###############################
+      speed = abs(speed)
+
+      if now_speed < speed:
+        #スピードアップ
+        now_speed = now_speed+10
+        if now_speed > MAX_MOTOR_SPEED:
+          now_speed = MAX_MOTOR_SPEED
+        self.pi.set_PWM_dutycycle( GPIO_MOTOR_PWM, now_speed//10 )
+
+      elif now_speed > speed:
+        #スピードダウン
+        now_speed = now_speed-30
+        if now_speed < MIN_MOTOR_SPEED:
+          now_speed = MIN_MOTOR_SPEED
+        self.pi.set_PWM_dutycycle( GPIO_MOTOR_PWM, now_speed//10 )
+
+
+      # ステアリング-コントローラ制御
+      ###############################
+      if self.sixaxis_switch.value == 0:
+
+        duty  = STEERING_FORWORD + (self.drive_steer.value*30) 
 
         if duty < STEERING_LEFT:
           duty = STEERING_LEFT 
@@ -338,12 +309,36 @@ class auto_cruise:
         # ステアリングを制御
         if now_steer < duty:
           now_steer = now_steer + 1
-          self.pi.set_servo_pulsewidth( GPIO_STEERING_PWM,	now_steer )
+          self.pi.set_servo_pulsewidth( GPIO_STEERING_PWM,	now_steer//10 )
         elif now_steer > duty:
           now_steer = now_steer - 1
-          self.pi.set_servo_pulsewidth( GPIO_STEERING_PWM,	now_steer )
-      
-      time.sleep(0.002)
+          self.pi.set_servo_pulsewidth( GPIO_STEERING_PWM,	now_steer//10 )
+
+      # ステアリング-自動制御
+      ###############################
+      else:
+        center  = self.drive_centering.value
+        duty    = self.drive_steer.value
+
+        #車体がラインに対して左右に曲がっている場合の調整
+        if duty < STEER_ANGLE_MIN & STEER_ANGLE_MAX < duty:
+          now_steer += int(duty*COEF_ANGLE)
+
+        #車体がライン中心に対してずれている場合の調整
+        if center < LINE_GAP_MIN & LINE_GAP_MAX < center:
+          now_steer += int(center*COEF_CENTERING)
+
+        #丸め込み
+        if now_steer < STEERING_LEFT:
+           now_steer = STEERING_LEFT 
+        elif now_steer > STEERING_RIGHT:
+          now_steer = STEERING_RIGHT
+
+        self.pi.set_servo_pulsewidth( GPIO_STEERING_PWM,	now_steer//10 )
+
+        print( (duty*COEF_ANGLE), self.drive_steer.value, now_steer)
+
+      time.sleep(0.001)
 
     # 停止
     print( "__driving stop" )
@@ -366,25 +361,27 @@ class auto_cruise:
     cap.set(cv2.CAP_PROP_FRAME_WIDTH,  CAM_SETTING_WIDTH )
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, CAM_SETTING_HEIGHT)
 
-    #モーターパワーオン
-    self.pi.write( GPIO_MOTOR_STBY, 1 )
+    x1 = ( CAM_ADJUST_WIDTH, CAM_ADJUST_HEIGHT )
+    x2 = ( CAM_SETTING_WIDTH - CAM_ADJUST_WIDTH, CAM_ADJUST_HEIGHT )
+    x3 = ( CAM_SETTING_WIDTH, CAM_SETTING_HEIGHT )
+    x4 = ( 0, CAM_SETTING_HEIGHT )
 
     while self.drive_active.value == DRIVE_START:
 
       # 画像を読み込む 
       ret, src = cap.read()
 
-      # 台形変換
-      pts1    = np.float32([[640,480],[385,150],[255,150],[0,480]])
-      pts2    = np.float32([[640,480],[640,0],[0,0],[0,480]])
-      M       = cv2.getPerspectiveTransform(pts1,pts2)
-      dst     = cv2.warpPerspective(src,M,(640,480))
-
       # グレースケールに変換
-      gray = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
+      gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+
+      # 台形変換
+      pts1    = np.float32( [x1,x2,x3,x4] )
+      pts2    = np.float32([[0,0],[CAM_SETTING_WIDTH,0],[CAM_SETTING_WIDTH,CAM_SETTING_HEIGHT],[0,CAM_SETTING_HEIGHT]])
+      M       = cv2.getPerspectiveTransform(pts1,pts2)
+      dst     = cv2.warpPerspective(gray,M,(CAM_SETTING_WIDTH, CAM_SETTING_HEIGHT))
 
       # ２値化
-      retval, bw = cv2.threshold(gray, 10, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+      retval, bw = cv2.threshold(dst, 10, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
       # 白黒反転
       bw = cv2.bitwise_not(bw)
@@ -392,7 +389,9 @@ class auto_cruise:
       # 輪郭を抽出
       img, contours, hierarchy = cv2.findContours(bw, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
 
-      if 1:
+      if self.sixaxis_switch.value == 0:
+        result = 'no lane'
+
         # 取得した輪郭の中からレーンを判定する
         for i in range(0, len(contours)):
 
@@ -400,7 +399,7 @@ class auto_cruise:
           area = cv2.contourArea(contours[i])
 
           # ノイズ（小さすぎる/大きすぎる領域）を除外
-          if area < 1e4 or 1e5 < area:
+          if area < PIC_THRESH_MIN or PIC_THRESH_MAX < area:
             continue
 
           # 矩形からレーンを検出する
@@ -410,7 +409,7 @@ class auto_cruise:
           # レーンのベクトルを計算する
           if result == 'find lane':
             #矩形描画
-            cv2.drawContours( gray,[box],0,(0,0,255),2 )   
+            cv2.drawContours( dst,[box],0,(0,0,255),2 )   
 
             # 輪郭データを浮動小数点型の配列に格納
             X = np.array(contours[i], dtype=np.float).reshape((contours[i].shape[0], contours[i].shape[2]))
@@ -418,22 +417,16 @@ class auto_cruise:
             # PCA解析
             mean, eigenvectors = cv2.PCACompute(X, mean=np.array([], dtype=np.float), maxComponents=1)
 
-            pt = (mean[0][0], mean[0][1])
+            pt  = (mean[0][0], mean[0][1])
             vec = (eigenvectors[0][0], eigenvectors[0][1])
-            #self.__drawAxis( gray, pt, vec, (255, 255, 0), 150)
             self.__cruiseCntrol( pt, vec )
             break
 
       # 表示
-      #cv2.imshow('input', src )
-      cv2.imshow('output', gray )
+      #cv2.imshow('input', gray ) #
+      cv2.imshow('output', dst )
 
       key = cv2.waitKey(1)
-      if key != -1:
-        self.drive_active.value = DRIVE_STOP
-
-    #モーターストップ
-    self.pi.write( GPIO_MOTOR_STBY, 0 )
 
     # 終了処理
     cap.release()
@@ -452,54 +445,65 @@ class auto_cruise:
 #####################################################################
   def __sixaxis(self):
 
-    motor_driver = 0
-    wait_counter = 0
     device_path  = '/dev/input/js0'
-
 
     with open(device_path, 'rb') as device:
       event = device.read( EVENT_SIZE )
 
       while True:
-        if event:
-          wait_counter = 0
-        else: 
-          if wait_counter < 120:
-            wait_counter += 1
-            print("sixaxis connecting wait...")
-            time.sleep(1)
-          else:
-            print("timeout sixaxis connecting")
-            break
 
         # イベント取得
         (ds3_time, ds3_val, ds3_type, ds3_num) = struct.unpack(EVENT_FORMAT, event)
 
-        # プッシュボタン検出
+        # プッシュボタン入力検出
+        ############################
         if ds3_type == 1:
+
           # select検出
           if ds3_num == 0:
+            ###########################
+            # 各プロセスの停止、動作完了
+            ###########################
+            print("motor driver off")
             self.pi.write( GPIO_MOTOR_STBY, 0 )
             self.pi.stop()
             self.drive_active.value = DRIVE_STOP
-            break			
+            break
 
-          # start検出
+          # Start検出
           elif ds3_num == 3:
-            if motor_driver == 0:
-              motor_driver = 1
-              print("motor driver on")
+            #モータードライバ電源ON
+            self.pi.write( GPIO_MOTOR_STBY, 1 )
+            print("motor driver on")
 
-            self.pi.write( GPIO_MOTOR_STBY, motor_driver )
+          # R2検出
+          elif ds3_num == 9:
+            self.sixaxis_switch.value = 0
+            print("sixaxis OFF")
 
-        # アナログセンサ検出
+          # R1検出
+          elif ds3_num == 11:
+            self.sixaxis_switch.value = 1
+            self.drive_motor.value    = 0
+            self.drive_steer.value    = 0
+            print("sixaxis ON")
+
+
+        # アナログ入力検出
+        ############################
         elif ds3_type == 2:
-          # Right Stick (horizontal)
-          if ds3_num == 2:
-            self.drive_steer.value = ds3_val//256
-          # Left Stick (vartical)        
-          elif ds3_num == 1:
-            self.drive_motor.value = ds3_val//32
+
+          if self.sixaxis_switch.value == 1:
+
+            # Right Stick (horizontal)
+            if ds3_num == 2:
+              if -32000 < ds3_val & ds3_val < 32000:
+                self.drive_steer.value = ds3_val//25
+
+            # Left Stick (vartical)        
+            elif ds3_num == 1:
+              if -32000 < ds3_val & ds3_val < 32000:
+                self.drive_motor.value = -1*(ds3_val//32)
 
         event = device.read(EVENT_SIZE)
 
@@ -511,42 +515,25 @@ class auto_cruise:
 
 
 #####################################################################
-#	@fn		    Manual_ctrl
+#	@fn		    Auto_ctrl
 #	@param		無し
-#	@brief		コントローラによるマニュアル走行制御メソッド
+#	@brief		カメラ画像による自動走行制御メソッド
 #####################################################################
-  def Manual_ctrl(self):
-
-    # 走行制御プロセスを生成
-    driving = Process(target = self.__driving, args=() )
-    driving.start()
+  def Auto_ctrl(self):
 
     # SIXAXIS制御プロセスを生成
     sixaxis = Process(target = self.__sixaxis, args=() )
     sixaxis.start()
 
-    driving.join()
-    sixaxis.join()
-
-
-
-
-
-#####################################################################
-#	@fn		    Auto_ctrl
-#	@param		無し
-#	@brief		カメラ画像によるJ自動走行制御メソッド
-#####################################################################
-  def Auto_ctrl(self):
-
     # 走行制御プロセスを生成
     driving = Process(target = self.__driving, args=() )
     driving.start()
 
-    # SIXAXIS制御プロセスを生成
+    # キャプチャ制御プロセスを生成
     capture = Process(target = self.__capture, args=() )
     capture.start()
 
+    sixaxis.join()
     driving.join()
     capture.join()
 
@@ -563,7 +550,6 @@ if __name__ == '__main__':
 
   car = auto_cruise()
 
-  #car.Manual_ctrl()
   car.Auto_ctrl()
 
 
